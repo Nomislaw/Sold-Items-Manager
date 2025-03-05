d8ae0cf2d6f2b103e2ca9b800f70b79b4249ccc8
Szymon <szymon.malinowski@outlook.com>
2025-03-05 23:50:36 +0100

    Baza danych

--- BLL/AppDbContext.cs
+++ BLL/AppDbContext.cs
@@ -5,7 +5,7 @@ namespace Halcube_Store_Manager_BLL
     public class AppDbContext : DbContext
     {
         //Tutaj ustawiamy parametry połączenia
-        string ConnectionParameters = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\szymo\\OneDrive\\Dokumenty\\Projekty\\Halcube Store Manager v0.4\\BLL\\Database1.mdf\";Integrated Security=True";
+        string ConnectionParameters = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\szymo\\OneDrive\\Dokumenty\\Projekty\\Test\\Sold-Items-Manager\\BLL\\Database1.mdf;Integrated Security=True";
         public DbSet<Competition>? Competitions { get; set; }
         public DbSet<Category>? Categories { get; set; }
         public DbSet<Product>? Products { get; set; }
